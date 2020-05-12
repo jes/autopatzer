a1bf5ab136fdc4801a8acaf6c9e6bfeebda26d27
James Stanley <james@incoherency.co.uk>
2020-05-12 16:02:11 +0100

    Revert "Move closures out of clock object"
    This reverts commit c5aad8f582727f7cdfca9a6705beb7571c9659f8.
    
    Broken & unncessary

--- electron/src/PlayGame/components/Timer.js
+++ electron/src/PlayGame/components/Timer.js
@@ -12,37 +12,6 @@ function pad(n, width, z) {
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
-const currentMillis = (millisecs) => {
-  if (ticking) {
-    millisecs -= Date.now() - startTime
-  }
-  return millisecs;
-};
-
-const evenTick = (millisecs) => {
-  let secs = Math.floor(currentMillis(millisecs) / 1000);
-  return secs%2 === 0;
-};
-
-const stringTime = (milisecs) => {
-  let ms = currentMillis(millisecs);
-
-  let hours = Math.floor(ms / (60*60*1000));
-  ms -= hours * 60*60*1000;
-  let mins = Math.floor(ms / (60*1000));
-  ms -= mins * 60*1000;
-  let secs = Math.floor(ms / 1000);
-  ms -= secs * 1000;
-
-  if (hours >= 1) { // >= 1 hour
-    return pad(hours, 2) + "h" + pad(mins, 2) + "m";
-  } else if (millisecs > 5*1000) { // >= 5 secs
-    return pad(mins, 2) + ":" + pad(secs, 2);
-  } else {
-    return pad(mins, 2) + ":" + pad(secs, 2) + "." + pad(ms, 3);
-  }
-};
-
 const Timer = ({ ticking, startTime, millisecs }) => {
   const [state, setState] = useState({
     millisecs: millisecs,
@@ -50,12 +19,44 @@ const Timer = ({ ticking, startTime, millisecs }) => {
     txt: '',
   });
 
+  const currentMillis = () => {
+    let ms = state.millisecs;
+    if (ticking) {
+      ms -= Date.now() - startTime
+    }
+    return ms;
+  };
+
+  const evenTick = () => {
+    let secs = Math.floor(currentMillis() / 1000);
+    return secs%2 === 0;
+  };
+
+  const stringTime = () => {
+    let ms = currentMillis();
+
+    let hours = Math.floor(ms / (60*60*1000));
+    ms -= hours * 60*60*1000;
+    let mins = Math.floor(ms / (60*1000));
+    ms -= mins * 60*1000;
+    let secs = Math.floor(ms / 1000);
+    ms -= secs * 1000;
+
+    if (hours >= 1) { // >= 1 hour
+      return pad(hours, 2) + "h" + pad(mins, 2) + "m";
+    } else if (state.millisecs > 5*1000) { // >= 5 secs
+      return pad(mins, 2) + ":" + pad(secs, 2);
+    } else {
+      return pad(mins, 2) + ":" + pad(secs, 2) + "." + pad(ms, 3);
+    }
+  };
+
   // reset state when parameters to the component change
   if (startTime !== state.startTime) {
     setState({
       millisecs: millisecs,
       startTime: startTime,
-      txt: stringTime(state.millisecs),
+      txt: stringTime(),
     });
   }
 
@@ -66,12 +67,12 @@ const Timer = ({ ticking, startTime, millisecs }) => {
   useInterval(() => {
     setState((state) => ({
       ...state,
-      txt: stringTime(state.millisecs),
+      txt: stringTime(),
     }));
   }, 250);
 
   return (
-    <Box align="center" text-align="center" boxShadow={1} style={{ backgroundColor:(ticking && evenTick(state.millisecs) ? '#edf3e8' : 'white') }}>
+    <Box align="center" text-align="center" boxShadow={1} style={{ backgroundColor:(ticking && evenTick() ? '#edf3e8' : 'white') }}>
       <Typography variant="h2" style={{ fontFamily: "Anonymous Pro" }}>
         {state.txt}
       </Typography>
